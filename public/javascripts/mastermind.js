d886029a6bf79b55291b8b0492ea09160a450a7d
Raph <falqonix@gmail.com>
2013-04-19 18:31:39 +0200

    CSS and Start Game Options in script

--- public/javascripts/mastermind.js
+++ public/javascripts/mastermind.js
@@ -1,17 +1,31 @@
-$(document).ready(function() {
-	$('#game').click(function () {
-			$.getJSON('play/', function(data) {
-  var items = [];
- 
-  $.each(data, function(key, val) {
-    items.push('<li id="' + key + '">' + val + '</li>');
-  });
+var mand = {};
+
+mand.gameSettings = function(size, dim, max) {
+	return {
+		getSize: function() { return size; },
+		getDim: function() { return dim; },
+		getMax: function() { return max; }
+	}
+};
+mand.rowWriter = function() {
 	
-  $('<ul/>', {
-    'class': 'my-new-list',
-    html: items.join('')
-  }).appendTo('#response');
-});
+};
+mand.gaming = $(document).ready(function() { 
+	$('#play').click(function () {		
+		if(typeof mand.gameSettings === 'function'){ //Jeśli jest funkcją to jeszcze nie zapisano danych, gra nie zaczęta			
+			mand.gameSettings = mand.gameSettings( $('#setSize').val(), $('#setDim').val(), $('#setMax').val() ); // Ustawienia w domknięciu		
+			var toGet = '/play/size/' + mand.gameSettings.getSize() + '/dim/' + 	//Tworzenie zapytania z odpowiednimi ustawieniami
+			mand.gameSettings.getDim() + '/max/' + mand.gameSettings.getMax() + '/';			
+			
+			$.getJSON(toGet, function(data) { 
+				$.each(data, function(key, val) { 
+					$('#game').prepend(val);
+				}); $('#play').text("Sprawdź"); 			
+			});
+		} else {
+			
+			
+		};		
 	});	
 });
 
--- routes/index.js
+++ routes/index.js
@@ -11,38 +11,49 @@ exports.play = function (req, res) {
         for (i = 0; i < puzzle.size; i += 1) {
             data.push(Math.floor(Math.random() * puzzle.dim));
         }
-        req.session.puzzle.data = data;
+			req.session.puzzle.data = data;
+			
         return {
-            'retMsg': 'Zaczynamy!'
+            'retMsg': 'Zaczynamy'
         };
     };
     // poniższa linijka jest zbędna (przy założeniu, że
     // play zawsze uzywany będzie po index) – w końcowym
     // rozwiązaniu można ją usunąć.
     req.session.puzzle = req.session.puzzle || req.app.get('puzzle');
-    /*
-     * req.params[2] === wartość size
-     * req.params[4] === wartość dim
-     * req.params[6] === wartość max
-     */
+   
     if (req.params[2]) {
         req.session.puzzle.size = req.params[2];
     }
 	if (req.params[4]) {
-        req.session.puzzle.size = req.params[4];
+        req.session.puzzle.dim = req.params[4];
     }
 	if (req.params[6]) {
-        req.session.puzzle.size = req.params[6];
+        req.session.puzzle.max = req.params[6];
     }
 	res.writeHead(200, {
 		'Content-Type': 'application/json; charset=utf8'});
     res.end(JSON.stringify(newGame()));
 };
 
+//Wyniki tylko po stronie serwera!!!
+// var correctNumber = function(){
+	// tab = [];
+	// return{
+		// setTab: function(rand){
+			// tab = rand;
+		// },
+		// getTab: function(){
+			// return tab;
+		// }
+	// }
+// }();
+
 exports.mark = function (req, res) {
     var markAnswer = function () {
         var move = req.params[0].split('/');
         move = move.slice(0, move.length - 1);
+		console.log(req.session.puzzle.data);
         return {
             'retVal': 'tutaj – zamiast tego napisu – ocena',
             'retMsg': 'coś o ocenie – np „Brawo” albo „Buuu”'
