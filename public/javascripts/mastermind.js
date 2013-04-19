ef6ff20f63665aa8af2d23118b97f40066c9d554
Raph <falqonix@gmail.com>
2013-04-19 21:55:07 +0200

    Mark Get request working

--- public/javascripts/mastermind.js
+++ public/javascripts/mastermind.js
@@ -7,26 +7,50 @@ mand.gameSettings = function(size, dim, max) {
 		getMax: function() { return max; }
 	}
 };
-mand.rowWriter = function() {
+
+mand.rowWriter = function(size) {
+	var sRow = '<tr>';
+	for(var i = 0; i < size; i += 1) {
+		sRow += '<td><input type="text" class="inputsNow"/></td>'
+	} sRow += '<td class="pointsNow"></td></tr>';
 	
+	$('#results').append(sRow);
 };
-mand.gaming = $(document).ready(function() { 
-	$('#play').click(function () {		
-		if(typeof mand.gameSettings === 'function'){ //Jeśli jest funkcją to jeszcze nie zapisano danych, gra nie zaczęta			
-			mand.gameSettings = mand.gameSettings( $('#setSize').val(), $('#setDim').val(), $('#setMax').val() ); // Ustawienia w domknięciu		
-			var toGet = '/play/size/' + mand.gameSettings.getSize() + '/dim/' + 	//Tworzenie zapytania z odpowiednimi ustawieniami
-			mand.gameSettings.getDim() + '/max/' + mand.gameSettings.getMax() + '/';			
-			
-			$.getJSON(toGet, function(data) { 
-				$.each(data, function(key, val) { 
-					$('#game').prepend(val);
-				}); $('#play').text("Sprawdź"); 			
-			});
-		} else {
-			
-			
-		};		
+
+$(document).ready(function() {
+	mand.start = $('#start').click(function () { //Zapisywanie ustawień		
+		mand.gameSettings = mand.gameSettings( $('#setSize').val(), $('#setDim').val(), $('#setMax').val() );	
+		
+		var playGet = '/play/size/' + mand.gameSettings.getSize() + '/dim/' +
+		mand.gameSettings.getDim() + '/max/' + mand.gameSettings.getMax() + '/';			
+		 
+		$.getJSON(playGet, function(data) { 
+			$.each(data, function(key, val) { 
+				$('#game').prepend(val);
+			});			 
+		});			
+		mand.rowWriter(mand.gameSettings.getSize());					
 	});	
+
+	mand.mark = $('#mark').click(function() {
+		var markGet = '/mark/';
+		
+		$('.inputsNow').each(function(index){
+			markGet += $(this).val() + '/';
+		});
+		
+		$.getJSON(markGet, function(data) { 
+			$.each(data, function(key, val) { 
+				$('.pointsNow').append(val);
+			});			
+		});
+
+		$('.inputsNow').attr('disabled', 'disabled').attr('class', 'inputsHist');
+
+		
+	});
+
 });
 
 
+//○○○●●●
